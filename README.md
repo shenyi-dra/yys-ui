a0e46772ef6b70d05b0e65590de0f3c744f8c6ee
shenyi <shenyi@dra100.com>
2019-11-15 11:11:17 +0800

    UI库基础脚手架-WIP

--- README.md
+++ README.md
@@ -8,7 +8,19 @@ npm install -g cross-env
 
 npm i element-theme -g
 
-## 开发模式
+# 文件夹目录
 
-npm run dev
-会
+dist 打包目录
+packages 自定义组件目录
+test 测试目录
+types TypeScript 申明目录
+
+## 打包开发环境
+
+npm run build OR npm run build:dev
+
+## 打开 webpack-dev-server 可将修改组件引入进行查看
+
+入口文件 src/main.js
+
+## 打包最终优化及亚索文件夹
--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,52 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
-// 读取配置文件
-const fs = require("fs");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = "development";
 }
 
-const sourceMap = process.env.NODE_ENV === "production" ? "soucre-map" : "eval";
-const env = "development";
+const sourceMap = "soucre-map";
+const env = process.env.NODE_ENV;
 
 const publicPath = "/";
+
+const performance = {
+  hints: "warning",
+  //入口起点的最大体积
+  maxEntrypointSize: 50000000,
+  //生成文件的最大体积
+  maxAssetSize: 30000000,
+  //只给出 js 文件的性能提示
+  assetFilter: assetFilename => {
+    return assetFilename.endsWith(".js");
+  },
+};
+
+/**
+ * Plugins
+ */
+const plugins = [
+  new HtmlWebpackPlugin({
+    chunks: ["app", "vendors"],
+    favicon: path.resolve(__dirname, "./src/favicon.ico"),
+    filename: "index.html",
+    template: path.resolve(__dirname, "./src/index.html"),
+    publicPath: publicPath,
+  }),
+  new MiniCssExtractPlugin({
+    filename: "assets/styles/[name].css",
+    chunkFilename: "assets/styles/[id].css",
+    ignoreOrder: false, // Enable to remove warnings about conflicting order
+  }),
+  new VueLoaderPlugin(),
+];
+
+if (env === "development") {
+  plugins.push(new webpack.HotModuleReplacementPlugin({}));
+}
 
 const webpackConfig = {
   mode: env,
@@ -24,7 +58,7 @@ const webpackConfig = {
   },
   output: {
     path: path.resolve(__dirname + "/dist"),
-    filename: "index.js",
+    filename: "[name].js",
     libraryTarget: "umd",
     publicPath,
   },
@@ -32,7 +66,7 @@ const webpackConfig = {
     extensions: [".vue", ".ts", ".tsx", ".js", ".jsx"],
     alias: {
       vue: "vue/dist/vue.min.js",
-      "@": path.resolve(__dirname, "./packages"),
+      "@packages": path.resolve(__dirname, "./packages"),
     },
   },
   module: {
@@ -118,12 +152,28 @@ const webpackConfig = {
             loader: "url-loader",
             options: {
               limit: 8192,
-              publicPath,
+              publicPath: "/assets/images/",
               outputPath: "/assets/images/",
             },
           },
         ],
       },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          ie8: true,
+        },
+      }),
+      new OptimizeCssAssetsPlugin({
+        cssProcessorOptions: {
+          safe: true,
+          discardComments: { removeAll: true },
+        },
+        canPrint: true,
+      }),
     ],
   },
   devServer: {
@@ -137,31 +187,17 @@ const webpackConfig = {
       disableDotRule: true,
     },
     host: "127.0.0.1",
-    hot: true,
+    // hot: true,
     port: 8000,
     proxy: {},
     publicPath,
     contentBase: path.resolve(__dirname, "dist"),
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin({}),
-    new HtmlWebpackPlugin({
-      chunks: ["app", "vendors"],
-      favicon: path.resolve(__dirname, "./src/favicon.ico"),
-      filename: "index.html",
-      template: path.resolve(__dirname, "./src/index.html"),
-      publicPath: publicPath,
-    }),
-    new MiniCssExtractPlugin({
-      filename: "assets/styles/[name].css",
-      chunkFilename: "assets/styles/[id].css",
-      ignoreOrder: false, // Enable to remove warnings about conflicting order
-    }),
-    new VueLoaderPlugin(),
-  ],
+  plugins,
+  performance,
 };
 module.exports = (env, argv) => {
-  webpackConfig.devtool = sourceMap;
+  // webpackConfig.devtool = sourceMap;
 
   return webpackConfig;
 };
